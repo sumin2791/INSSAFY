2d5733026353626886f53140c6ba69d83556d956
배용렬 <raon0725@naver.com>
2021-02-01 11:50:05 +0900

    [frontend]기타: backend 폴더 추가

---
+++ backend/src/main/java/com/ssafy/pjt1/model/service/main/MainServiceImpl.java
@@ -0,0 +1,22 @@
+package com.ssafy.pjt1.model.service.main;
+
+import java.util.List;
+
+import com.ssafy.pjt1.controller.SubscriptionDto;
+import com.ssafy.pjt1.model.mapper.MainMapper;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MainServiceImpl implements MainService {
+
+    @Autowired
+    public SqlSession sqlSession;
+
+    @Override
+    public List<SubscriptionDto> selectFavorite(String user_id) {
+        return sqlSession.getMapper(MainMapper.class).selectFavorite(user_id);
+    }
+}
