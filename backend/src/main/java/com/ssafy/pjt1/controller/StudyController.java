dc4696d1427e743c0f5aa9984c7b1621013a5765
윤수민 <sumin9497@naver.com>
2021-02-03 00:10:48 +0900

    [backend]기능추가: 스터디 모집글 리스트

--- backend/src/main/java/com/ssafy/pjt1/controller/StudyController.java
+++ backend/src/main/java/com/ssafy/pjt1/controller/StudyController.java
@@ -1,8 +1,18 @@
 package com.ssafy.pjt1.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.ssafy.pjt1.model.service.study.StudyService;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,5 +26,34 @@ public class StudyController {
     private static final String SUCCESS = "success";
     private static final String FAIL = "fail";
 
+    @Autowired
+    private StudyService studyService;
+
+    /*
+     * 기능: 스터디 모집글 리스트
+     * 
+     * developer: 윤수민
+     * 
+     * @param : 
+     * 
+     * @return : message, postList(post_id,user_id,post_date,post_title,post_description,
+     * post_image,post_iframe,post_header,post_state,like_count, comment_count)
+     */
+    @GetMapping("/getPromoList")
+    public ResponseEntity<Map<String, Object>> getPromoList(){
+        Map<String, Object> resultMap = new HashMap<>();
+        HttpStatus status = HttpStatus.ACCEPTED;
+        logger.info("post/getPostList 호출성공");
+        try {
+            List<Map<String, Object>> postList = studyService.getPromoList(); 
+            resultMap.put("postList", postList);          
+            resultMap.put("message", SUCCESS);
+        } catch (Exception e) {
+            logger.error("실패", e);
+            resultMap.put("message", FAIL);
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+        return new ResponseEntity<Map<String, Object>>(resultMap, status);
+    }
     
 }
