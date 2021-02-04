b015c62347654089dfbb228f8ca23663938187f2
최규수 <qsoo3810@gmail.com>
2021-02-04 16:48:09 +0900

    [frontend] 기능변경: 보드 전체리스트 및 검색 리스트 디자인 변경

--- frontend/src/views/search/SearchBoard.vue
+++ frontend/src/views/search/SearchBoard.vue
@@ -1,24 +1,74 @@
 <template>
-  <v-app id="inspire">
+  <v-app>
 
     <v-main class="main-bg-color">
       <v-container>
-        <v-row>
-          <!-- 검색어 결과 노출 부분 -->
+        <!-- 검색어 결과 노출 부분 -->
+        <v-row>          
           <v-col 
-          class="text-center
-            text-body-2">
-            보드 중 "{{ searchKeyword }}"에 대한 검색 결과: {{ searchCount }} 건
+            class="mb-4
+            text-center
+              text-body-1">
+              보드 중 "{{ search.searchKeyword }}"에 대한 검색 결과 &nbsp; '{{ search.searchCount }}건'
           </v-col>
-          <template v-for="n in 4">
-            <Board 
-              v-for="n in 6"
-              :key="`${n}`"
-              cols="6"
-              md="2"
-            />{{n}}
-          </template>
         </v-row>
+        <!-- 검색 건수 / 정렬 / 보드 생성 -->
+        <div 
+          class="d-flex 
+            flex-row
+            justify-space-between"
+        >
+          <!-- 검색 건수 / 정렬 -->
+          <div>
+            <v-select
+              v-model="search.currentSort"
+              :items="sortResult"
+              label="정렬기준"
+              solo
+              color="##fff"
+              class="sort-dropdown"
+              block
+            ></v-select>
+          </div>
+          
+          <!-- 보드 생성 버튼 -->
+          <div>
+            <v-btn
+              color="brown darken-1"
+            >
+              <v-icon
+                color="#fff"
+              >
+                mdi-plus
+              </v-icon>
+              <div class="text-white">보드 만들기</div>
+            </v-btn>
+          </div>
+        </div>
+      <!-- 보드 보여주는 부분 -->
+      <v-row >
+        <v-col class="col-6 col-sm-3">
+          <Board />
+        </v-col>
+        <v-col class="col-6 col-sm-3">
+          <Board />
+        </v-col>
+        <v-col class="col-6 col-sm-3">
+          <Board />
+        </v-col>
+        <v-col class="col-6 col-sm-3">
+          <Board />
+        </v-col>
+        <v-col class="col-6 col-sm-3">
+          <Board />
+        </v-col>
+        <v-col class="col-6 col-sm-3">
+          <Board />
+        </v-col>
+        <v-col class="col-6 col-sm-3">
+          <Board />
+        </v-col>
+      </v-row>
       </v-container>
     </v-main>
   </v-app>
@@ -34,8 +84,13 @@ export default {
   },
   data() {
     return {
-      searchKeyword: '게임',
-      searchCount: 248,
+      search: {
+        searchKeyword: '게임',
+        searchCount: 248,
+        currentSort: '',
+      },
+      // 검색 정렬 조건
+      sortResult: ['최신순', '인기순'],
     }
   },
   methods: {
@@ -49,5 +104,23 @@ export default {
 <style scoped>
 .main-bg-color {
   background-color: #ebebe9;
+}
+.sort-dropdown {
+  max-width: 50%;
+}
+/* 검색결과들 담는 컨테이너 */
+.search-result {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 0;
+  padding-top: 0;
+  flex-flow: wrap;
+  border: 3px solid #000000;
+}
+/* 검색보드 감싸는 div */
+.search-result-board {
+  flex-basis: 19.5%;
+  border: 3px dotted #000000;
 }
 </style>
