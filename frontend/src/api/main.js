f5117a4761efdaf6fcf0ecd827307d0622b969fd
윤수민 <sumin9497@naver.com>
2021-02-12 00:18:24 +0900

    [backend]삭제: front 폴더 삭제

--- frontend/src/api/main.js
+++
@@ -1,9 +0,0 @@
-import http from './http';
-
-export function getFavorites(user_id) {
-  return http.get(`/selectFavorite/${user_id}`);
-}
-
-export function getFollowRank() {
-  return http.get('/getFollowRank');
-}
