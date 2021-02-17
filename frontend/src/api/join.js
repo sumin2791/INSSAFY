071f558c1afcf2f870dd8a7d6afb4a469355a8da
배용렬 <raon0725@naver.com>
2021-02-17 12:06:45 +0900

    clear

--- frontend/src/api/join.js
+++
@@ -1,5 +0,0 @@
-import http from '@/api/http';
-
-export function confirmSolution(solution) {
-  return http.get(`/account/confirm/quiz/${solution}`);
-}
--- frontend/src/api/search.js
+++
@@ -1,7 +0,0 @@
-import http from './http';
-
-export function getSearchAllBoard(payload) {
-  return http.get(
-    `/board/searchBoard?keyword=${payload.keyword}&page=${payload.page}&size=${payload.size}&sort=${payload.sort}&type=${payload.type}`
-  );
-}
