6b66a57ae776d9a33e879707932be7ac663392e7
배용렬 <raon0725@naver.com>
2021-02-17 12:23:15 +0900

    [frontend]기타: 배포를 위한 baswURL 변경

--- frontend/src/api/http.js
+++ frontend/src/api/http.js
@@ -4,12 +4,12 @@ import store from '../vuex/store';
 
 // axios 인스턴스를 만들 때 구성 기본 값 설정
 const instance = axios.create({
-  baseURL: 'http://localhost:8000/',
-  // baseURL: 'http://http://i4c109.p.ssafy.io/api',
+  // baseURL: 'http://localhost:8000/',
+  baseURL: 'http://http://i4c109.p.ssafy.io/api',
 });
 
 //request 요청 전에 header에 토큰값을 넣기 위한 interceptor
-instance.interceptors.request.use(function(config) {
+instance.interceptors.request.use(function (config) {
   if (store.state.auth.user.token !== null) {
     config['headers'] = {
       // Authorization: `Bearer ${store.state.auth.user.token}`,
@@ -24,13 +24,13 @@ instance.interceptors.request.use(function(config) {
 //catch 안에처 처리할 error 메시지 interceptor
 instance.interceptors.response.use(
   //status === 200 일 때, validationerror를 빈 json 상태로 변경
-  function(response) {
+  function (response) {
     store.commit('error/setErrorMessages', {});
     return response;
   },
   //status === 422(검증 에러)일 때 validationError 값 없데이트
   //422가 아닐 경우 error vue.js에서 fetch로 받을 수 있도록 reject 처리
-  function(errors) {
+  function (errors) {
     console.log(`errors>>${errors}`);
     //status null일 경우 토큰 재발급
     // if (errors.response == undefined) {
